75bd56c51adad69d0a145fecc00ef22becb4e8fd
Truong Anh Vu <truonganhvu205@gmail.com>
2025-09-16 02:58:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+```
+npm init
+
+npm i express
+npm i epress-handlebars
+
+npm i nodemon --save-dev
+npm i morgan --save-dev
+npm i sass --save-dev
+```
+
+```
+"scripts": {
+...."start": "nodemon --inspect src/index.js",
+...."watch": "sass src/resources/scss:src/public/css --watch",
+},
+```
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const path = require('path')
+
+const app = express()
+
+app.use(morgan('combined'))
+
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, './resources/views'))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.listen(3000)
